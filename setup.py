027ec3b33a008b9cfcbfdc3f475970e3ea755cf8
shadownjoke <80283262+shadownjoke@users.noreply.github.com>
2024-03-14 11:37:08 +0200

    Update setup.py
    updated

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from distutils.core import setup
 from setuptools import find_packages
-from spdk import __version__
 
 
-setup(name='spdk', version=__version__, packages=find_packages())
+setup(name='clothsegment', version="1.0", packages=find_packages())
